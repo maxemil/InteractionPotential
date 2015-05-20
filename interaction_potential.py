4209ad851ed76287561fd5cc3a088266d4aa4ace
Empetrum <empetrum@linux-euqo.site>
2015-05-20 12:00:47 +0200

    create template functions

--- interaction_potential.py
+++ interaction_potential.py
@@ -1,13 +1,40 @@
 #! /usr/bin/env python3
 
 from Bio import PDB
+import argparse
+import threading
 
-def filter_ligands:
+
+def filter_ligands():
     return chain
 
+def is_connected():
+    return false
+
 def align_structs(ref, sample):
-    return RMSD
+    ref_atoms = []
+    smpl_atoms = []
+
+    for ref_res in ref:
+        ref_atoms.append(ref_res['CA'])
+    for smpl_res in sample:
+        smpl_atoms.append(smpl_res['CA'])
+
+    super_imposer = PDB.Superimposer()
+    super_imposer.set_atoms(ref_atoms, smpl_atoms)
+    super_imposer.apply(sample.get_atoms())
+    
+    return super_imposer.rms
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help='folder containing the crystal\
+        structure and the predictions', type=str)
+    parser.add_argument('ref', help='name of the file containing the crystal\
+        structure', type=str)
+    args = parser.parse_args()
+    
+    # test
+    align_structures(args.ref, args.ref)
 
