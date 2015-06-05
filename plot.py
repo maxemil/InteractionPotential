dbed5e31a5d9066bf55773181de7504c4dbc9e18
yapph <yapphdorlw@gmail.com>
2015-06-05 19:03:30 +0200

    First comparison

--- plot.py
+++ plot.py
@@ -3,19 +3,49 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.stats import pearsonr, spearmanr
 
 
-def histogram(data, title):
+def histogram(data, title, addition, unit="kcal/mol"):
     plt.clf()
     plt.hist(data, bins=25)
-    plt.title("Distribution of Energies")
-    plt.xlabel("kcal/mol")
+    plt.title("Distribution of {}".format(addition))
+    plt.xlabel(unit)
     plt.suptitle("Dataset: {}".format(title))
-    plt.savefig("results/{}.pdf".format(title))
+    plt.savefig("results/{}_{}.pdf".format(addition, title))
+
+
+def rmsds_cmp(subset, energies, rmsds):
+    #Convert names in rmsds
+    def shorten(string):
+        return string.split("/")[-1]
+    rmsds["PDB"] = rmsds["PDB"].apply(shorten)
+    # Unify
+    matrix = pd.DataFrame.join(energies.set_index("PDB"),
+                               rmsds.set_index("PDB"), how="inner")
+    # Print results
+    print("Subset {}".format(subset))
+
+    # Value correlation
+    print("Pearson Corelation (values): {}".format(pearsonr(
+        matrix["RMSD"], matrix["Energy in kcal/mol"])[0]))
+    print("Spearman Corelation (rank): {}".format(spearmanr(
+        matrix["RMSD"], matrix["Energy in kcal/mol"])[0]))
+
+    print("Top 5 according to RMSD")
+    print(matrix.sort("RMSD").iloc[0:5])
+    print("Top 5 according to Interaction Potential")
+    print(matrix.sort("Energy in kcal/mol").iloc[0:5])
+
 
 
 sets = ["T0762", "T0769", "T0776", "T0784"]
 
-for x in sets:
-    frame = pd.read_csv("results/{}.csv".format(x))
-    histogram(frame[frame.columns[1]], x)
+for subset in sets:
+    energies = pd.read_csv("results/{}.csv".format(subset))
+    rmsds = pd.read_csv("results/rmsds_{}".format(subset), sep="\t")
+
+    histogram(energies["Energy in kcal/mol"], subset, "Energies")
+    histogram(rmsds["RMSD"], subset, "RMSDs", unit="Å")
+
+    rmsds_cmp(subset, energies, rmsds)
